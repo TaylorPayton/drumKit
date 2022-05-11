c688ebc548238463a0db651d93001a4d987b3834
DemonKing <91680798+TaylorPayton@users.noreply.github.com>
2022-05-11 09:24:56 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,50 +1,73 @@
-var buttons = document.querySelectorAll(".drum");
+function mainFunction(){
+var randomNum = Math.floor((Math.random()*6)+1);
 
-for(i=0;i<buttons.length;i++){
-  buttons[i].addEventListener('click', handleClick);
+var randomNumTwo = Math.floor((Math.random()*6)+1);
+
+if(randomNum == 1){
+
+  document.querySelector("img.img1").src="pictures/dice1.png";
 }
-document.addEventListener('keydown', handleClick);
- function handleClick(e){
-   var buttonInnerHtml = this.innerHTML;
-   var buttonPressInnerHtml = e.key;
-   switch(buttonInnerHtml||buttonPressInnerHtml){
-     case "w":
-      alert("w button is working");
-      var tom1 = new Audio('sounds/tom-1.mp3');
-      tom1.play();
-      break
-      case "a":
-        alert("a button is working");
-        var tom2 = new Audio('sounds/tom-2.mp3');
-        tom2.play();
-        break
-      case "s":
-        alert("s button is working");
-        var tom3 = new Audio("sounds/tom-3.mp3");
-        tom3.play();
-        break
-      case "d":
-        alert("d button is working");
-        var tom4 = new Audio("sounds/tom-4.mp3");
-        tom4.play();
-        break
-      case "j":
-        alert("j button is working");
-        var crash = new Audio("sounds/crash.mp3");
-        crash.play();
-        break
-      case "k":
-        alert("k button is working");
-        var kickbass = new Audio("sounds/kick-bass.mp3");
-        kickbass.play();
-        break
-      case "l":
-        alert("l button is working");
-        var snare = new Audio("sounds/snare.mp3");
-        snare.play();
-        break
-      default:
-          alert(buttonInnerHtml)
+else if(randomNum == 2 ){
 
- }
+  document.querySelector("img.img1").src="pictures/dice2.png";
+}
+else if(randomNum == 3 ){
+
+  document.querySelector("img.img1").src="pictures/dice3.png";
+}
+else if(randomNum == 4 ){
+
+  document.querySelector("img.img1").src="pictures/dice4.png";
+}
+else if(randomNum == 5 ){
+
+  document.querySelector("img.img1").src="pictures/dice5.png";
+}
+else if(randomNum == 6 ){
+
+  document.querySelector("img.img1").src="pictures/dice6.png";
+}
+else{
+  console.log("wrong");
+}
+
+
+if(randomNumTwo == 1){
+
+  document.querySelector("img.img2").src="pictures/dice1.png";
+}
+else if(randomNumTwo == 2 ){
+
+  document.querySelector("img.img2").src="pictures/dice2.png";
+}
+else if(randomNumTwo == 3 ){
+
+  document.querySelector("img.img2").src="pictures/dice3.png";
+}
+else if(randomNumTwo == 4 ){
+
+  document.querySelector("img.img2").src="pictures/dice4.png";
+}
+else if(randomNumTwo == 5 ){
+
+  document.querySelector("img.img2").src="pictures/dice5.png";
+}
+else if(randomNumTwo == 6 ){
+
+  document.querySelector("img.img2").src="pictures/dice6.png";
+}
+else{
+  console.log("wrong");
+}
+
+if(randomNum > randomNumTwo){
+  document.querySelector("h1").innerHTML=" 🚩Player 1 Wins";
+}
+else if(randomNum < randomNumTwo){
+  document.querySelector("h1").innerHTML=" Player 2 Wins🚩";
+}
+else if(randomNum == randomNumTwo){
+  document.querySelector("h1").innerHTML=" Draw!";
+}
+
 }
