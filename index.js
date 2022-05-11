8236f855a59d924267d3423805f0dc5b5a788d92
DemonKing <91680798+TaylorPayton@users.noreply.github.com>
2022-05-11 09:20:11 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -20,27 +20,27 @@ document.addEventListener('keydown', handleClick);
         break
       case "s":
         alert("s button is working");
-        var tom3 = new Audio('sounds/tom-3.mp3');
+        var tom3 = new Audio("sounds/tom-3.mp3");
         tom3.play();
         break
       case "d":
         alert("d button is working");
-        var tom4 = new Audio('sounds/tom-4.mp3');
+        var tom4 = new Audio("sounds/tom-4.mp3");
         tom4.play();
         break
       case "j":
         alert("j button is working");
-        var crash = new Audio('sounds/crash.mp3');
+        var crash = new Audio("sounds/crash.mp3");
         crash.play();
         break
       case "k":
         alert("k button is working");
-        var kick-bass = new Audio('sounds/kick-bass.mp3');
-        kick-bass.play();
+        var kickbass = new Audio("sounds/kick-bass.mp3");
+        kickbass.play();
         break
       case "l":
         alert("l button is working");
-        var snare = new Audio('sounds/snare.mp3');
+        var snare = new Audio("sounds/snare.mp3");
         snare.play();
         break
       default:
